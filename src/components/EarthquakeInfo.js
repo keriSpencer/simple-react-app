31b086ff44a02fd59869b765eecc9c3e836d0486
Keri Spencer <kerigspencer@gmail.com>
2017-08-16 18:51:29 -0400

    Finished project

---
+++ src/components/EarthquakeInfo.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import '/Users/kerispencer/Development/react/simple-react-app/src/styles/App.css'
+import moment from 'moment'
+
+class EarthquakeInfo extends Component {
+  render() {
+    return (
+      <div className="earthquake-title">
+        This is a list of 8 Earthquakes occurring on the morning of July 14th across the United States.
+      </div>
+    )
+  }
+}
+
+export default EarthquakeInfo
---
+++ src/components/EarthquakeList.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import '/Users/kerispencer/Development/react/simple-react-app/src/styles/App.css'
+import earthquakes from '../data/earthquakes.js'
+import moment from 'moment'
+import '/Users/kerispencer/Development/react/simple-react-app/src/styles/App.css'
+import '/Users/kerispencer/Development/react/simple-react-app/src/styles/index.css'
+
+class EarthquakeList extends Component {
+  render() {
+    let quakes = earthquakes.features.map(earthquake => {
+      return (
+        <div>
+          <div className="col-sm-6" key={earthquake.id}>
+            <div className="card">
+              <div className="card-block">
+                <h4 className="card-title">
+                  {earthquake.properties.place}
+                </h4>
+                <h6 className="card-subtitle mb-2 text-muted">
+                  Magnitude: {earthquake.properties.mag}
+                </h6>
+                <h6 className="card-subtitle mb-2 text-muted">
+                  Time: {moment(earthquake.time).format('llll')}
+                </h6>
+                <p className="card-text">
+                  Coordinates: {earthquake.geometry.coordinates}
+                </p>
+
+                <a href={earthquake.properties.url} className="card-link">
+                  USGS Event Link
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      )
+    })
+    return (
+      <div className="quake-list">
+        <div className="row">
+          {quakes}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default EarthquakeList
